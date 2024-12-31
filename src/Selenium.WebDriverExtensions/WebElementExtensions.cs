a55a97f275a4563a1791ad39bdb8edee1604541a
David S. <72085557+another-one-employee@users.noreply.github.com>
2024-12-31 18:43:57 +0300

    Add WebElementExtensions (#4)

---
+++ src/Selenium.WebDriverExtensions/WebElementExtensions.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Selenium.WebDriverExtensions
+{
+    public static class WebElementExtensions
+    {
+        public static void TypeWithDelay(IWebElement element, string text, int delayInMs)
+        {
+            foreach (char c in text)
+            {
+                element.SendKeys(c.ToString());
+                Thread.Sleep(delayInMs);
+            }
+        }
+
+        public static void TypeWithDelay(IWebElement element, string text, TimeSpan delay)
+        {
+            foreach (char c in text)
+            {
+                element.SendKeys(c.ToString());
+                Thread.Sleep(delay);
+            }
+        }
+
+        public static async Task TypeWithDelayAsync(IWebElement element, string text, int delayInMs)
+        {
+            foreach (char c in text)
+            {
+                element.SendKeys(c.ToString());
+                await Task.Delay(delayInMs);
+            }
+        }
+
+        public static async Task TypeWithDelayAsync(IWebElement element, string text, TimeSpan delay)
+        {
+            foreach (char c in text)
+            {
+                element.SendKeys(c.ToString());
+                await Task.Delay(delay);
+            }
+        }
+    }
+}
